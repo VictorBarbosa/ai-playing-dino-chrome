88e2a06c8a05ef3e9c1e8b24b180c2e14a46ceb5
VictorBarbosa <victor.hugo.p.barbosa@hotmail.com>
2023-08-26 17:03:17 -0400

    publish

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,19 @@ import { Component, OnInit, } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    document.onkeydown = function (evt) {
+      evt = evt || window.event;
+      if (evt.keyCode == 32) {
+        var box = document.getElementById("messageBox");
+        if (box) {
+          (box as any).style.visibility = "hidden";
+        }
+      }
+    };
+
+  }
 
   ngOnInit(): void { }
 
